de375e608f88cfc129f081d402664ab8e3a53ad8
ErgodicEntropy <129313892+ErgodicEntropy@users.noreply.github.com>
2026-07-14 15:26:00 +0100

    Enhance commands.sh with command explanations
    Updated commands.sh with detailed explanations for various shell commands and options.

--- commands.sh
+++ commands.sh
@@ -5,13 +5,17 @@ ls -alid
 pwd 
 cd [dir] ..
 mkdir -p
-rmdir
+rmdir -p
 rm -rf
 cp file1 file2
 mv file directory
 open 
 touch
+    touch -m: update modification time to now of an existing file otherwise create it
+    touch -t: modify modification time of an existing file
+    touch -h -t: modify the modification time of a symbolic/soft link
 cat
+    cat -e
 nano
 emacs
 vim
@@ -19,8 +23,8 @@ more
 less
 head
 tail
-ln file hardlink
-ln -s file softlink/symbolic link
+ln file hardlink #create a hard link (deep file copy): a hard link is a link that has the same inode (file metadata) as the original file -> even if original file is deleted, hard link persists because it's a physical pointer
+ln -s file softlink/symbolic link #symbolic or soft link (shallow file copy): a symbolic link is a virtual pointer to the original file (a shortcut) as it doesn't have the same inode but it increases the line count for itself and original file's -> if originall file is deleted, it gets deleted too.
 gzip
     gzip alone delets the original filee
     gzip -k keeps the original file intact
@@ -60,6 +64,12 @@ file execution:
     sh file.sh
     chmod +x file.sh followed by ./file.sh
 
+C compilation:
+    gcc -Wextra -Wall -Werror prerequisites.c -o executable #compile and link
+    gcc -c -Wextra -Wall -Werror prerequisites.c -o executable #compile without link
+    ./executable #run/execute the file
+    gcc -c -Wextra -Wall -Werror prerequisites.c -o executable && ./executable #compile and execute
+
 diff a b #produces a patch file describing differences
 diff -u a b  #produces a patch file describing differences but more human-readable
 patch a < patchfile #modify a by overwriting its content with b content. NOTE: this doesn't redirect to stoud steam hence no redirection is possible
@@ -68,16 +78,22 @@ patch -R a < patchfile #reverse patching
 file -C -m magicfile #offset type value decription
 file -b -m magicdb.mgc filename
 #TEXT PROCESSING & SEARCH
-find -type f/d/l -size 2 -name "" -o
-grep
-sed
+find -type f/d/l/c/b/p/s -size +2/-2/2 -name "regex" -iname "regex" -mtime -atime -ctime -o -a -not/! -print -delete -exec [command]{} \;
+grep -i -n -r -v -c -l "textpattern" filename
+sed "s/original/new/2gi"
 awk
 crontab
+tr 'original' 'new'
+wc
 
 #USER & SYSTEM ADMINISTRATION
 man
 sudo
 su
+who
+whoami
+id
+groups
 
 #SYSTEM & PROCESS MONITORING
 top
@@ -101,9 +117,16 @@ ssh-agent -s: tells the ssh agent to output commands in bourne shell
 ssh -T git@gitrepositorylinkssh
 git clone git@gitrepolinkssh
 wget
+curl
 
 
 #miscellaneous
+
+##environment variables
+PWD
+HOME
+
+## assignments
 
 d = $(cat)
 
